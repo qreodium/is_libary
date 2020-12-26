5ec78a31619a0405290a0e9a26f75b4e45b3e124
Vladimir Shilov <deluxewhite@yandex.ru>
2020-12-26 14:30:42 +0700

    add config

---
+++ config.h
@@ -0,0 +1,15 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <QString>
+
+namespace Config
+{
+    static const QString nameApplication = "Библиотека";
+
+    static const QString fileUsers = "users.bin";
+    static const QString fileWorkers = "workers.bin";
+    static const QString fileBooks = "books.bin";
+}
+
+#endif // CONFIG_H
